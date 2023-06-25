c1ba2aab024016d46eb1795946638b4ef0488420
codeace110 <neri11032321@gmail.com>
2023-06-25 18:58:35 +0800

    first commit

---
+++ js/config/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCbT8mcO-zp_9-XTJgN56WhzedSGbHXsI4",
+  authDomain: "load2wallet.firebaseapp.com",
+  projectId: "load2wallet",
+  storageBucket: "load2wallet.appspot.com",
+  messagingSenderId: "284169236230",
+  appId: "1:284169236230:web:5564c8bb1caf819b7df887",
+  measurementId: "G-K2S8NWK22K"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
