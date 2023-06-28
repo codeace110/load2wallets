f8bae1df0ee9cb63e4049b60145ee999aa8c7914
codeace110 <neri11032321@gmail.com>
2023-06-28 20:21:25 +0800

    add google and facebook api

--- public/js/ballon.js
+++ public/js/ballon.js
@@ -35,3 +35,6 @@
 
   // Start the animation
   moveBalloon();
+
+
+  
--- public/js/login.js
+++ public/js/login.js
@@ -12,3 +12,21 @@ function login() {
   });
   promise.catch((err) => errorNotification(err.message));
 }
+
+// Function to handle Google login
+    function loginWithGoogle() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+
+      firebase.auth().signInWithPopup(provider)
+        .then(function(result) {
+          // Login successful, handle logged-in user
+          var user = result.user;
+          console.log("User logged in:", user);
+        })
+        .catch(function(error) {
+          // Handle errors
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.error("Google login error:", errorCode, errorMessage);
+        });
+    }
