3151b624a5cf971a1a9849c6daeac2b8c380f4de
codeace110 <neri11032321@gmail.com>
2023-07-01 01:40:17 +0800

    adding email verification steps

--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,5 @@
 let globalEmail = "";
 
-function login() {
-  console.log('Attempting to login user ...');
-  var userEmail = document.getElementById("email_field").value;
-  var userPass = document.getElementById("password_field").value;
-  const auth = firebase.auth();
-  const promise = auth.signInWithEmailAndPassword(userEmail, userPass);
-
-  promise.then((response) => {
-    globalEmail = response.user.email;
-    // Redirect to user.html
-    window.location.href = "./user.html";
-  });
-
-  promise.catch((err) => errorNotification(err.message));
-}
-
 // Function to handle login when Enter key is pressed
 function handleLoginKeyPress(event) {
   if (event.key === 'Enter') {
@@ -26,7 +10,6 @@ function handleLoginKeyPress(event) {
 // Add event listener to the password field
 document.getElementById("password_field").addEventListener("keydown", handleLoginKeyPress);
 
-
 // Function to show the loading GIF
 function showLoading() {
   document.getElementById("loading").style.display = "block";
@@ -35,6 +18,51 @@ function showLoading() {
 // Function to hide the loading GIF
 function hideLoading() {
   document.getElementById("loading").style.display = "none";
+}
+
+// Function to handle successful login
+function handleSuccessfulLogin(user) {
+  if (user.emailVerified) {
+    globalEmail = user.email;
+    // Redirect to user.html
+    window.location.href = "./user.html";
+  } else {
+    // Email not verified
+    sendVerificationEmail(user)
+      .then(() => {
+        successNotification("Verification email sent. Please check your email and verify your account.");
+        document.getElementById("send-verification-div").style.display = "block";
+      })
+      .catch((err) => {
+        errorNotification("Failed to send verification email. Please try again.");
+        console.error("Error sending verification email:", err);
+      });
+  }
+}
+
+// Function to handle failed login
+function handleFailedLogin(err) {
+  errorNotification(err.message);
+}
+
+// Function to log in user
+function login() {
+  console.log('Attempting to login user ...');
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+  const auth = firebase.auth();
+  const promise = auth.signInWithEmailAndPassword(userEmail, userPass);
+
+  promise
+    .then((response) => {
+      handleSuccessfulLogin(response.user);
+    })
+    .catch(handleFailedLogin);
+}
+
+// Function to send verification email
+function sendVerificationEmail(user) {
+  return user.sendEmailVerification();
 }
 
 // Google Sign-In function
@@ -49,10 +77,27 @@ function loginWithGoogle() {
 // Check if the user is already logged in
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    // User is already logged in, redirect to user.html
-    window.location.href = "./user.html";
+    handleSuccessfulLogin(user);
   } else {
-    // User is not logged in
     hideLoading(); // Hide the loading GIF since user is on the login/register page
   }
 });
+
+
+function resendVerificationEmail() {
+  const user = firebase.auth().currentUser;
+
+  if (user) {
+    sendVerificationEmail(user)
+      .then(() => {
+        successNotification("Verification email sent. Please check your email and verify your account.");
+      })
+      .catch((err) => {
+        errorNotification("Failed to send verification email. Please try again.");
+        console.error("Error sending verification email:", err);
+      });
+  } else {
+    errorNotification("No user found. Please login again.");
+  }
+}
+
